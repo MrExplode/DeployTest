95fcbe4e99ba583acc64b3db53358f4e68bd50c2
ExplodeBot <sunstorm@outlook.hu>
2019-04-09 21:21:39 +0200

    Fix #12
    I hate those relative and absolute path marks

--- scripts/javadoc.sh
+++ scripts/javadoc.sh
@@ -15,24 +15,22 @@ PROJECT_NAME="DeployTest"
 echo -e "\n\e[93mBuilding JavaDoc...\n"
 
 # Building the javadoc
+fold_start doc "Building javadoc"
 mvn javadoc:javadoc
+fold_end doc
 
 # Setting up git
 cd $HOME
-git config --global user.name "ExplodeBot"
-git config --global user.email "sunstorm@outlook.hu"
+git config --local user.name "ExplodeBot"
+git config --local user.email "sunstorm@outlook.hu"
 
 # Cloning webpage
 git clone --branch=master https://${GITHUB_TOKEN}@github.com/MrExplode/MrExplode.github.io website
 
 # seek into target folder
 cd website
-cd $HOME/target
-ls
-cd $HOME/website
-# continue
-git rm -rf /projects/$PROJECT_NAME/javadoc
-cp -Rf $HOME/target/apidocs /projects/$PROJECT_NAME/javadoc
+git rm -rf projects/$PROJECT_NAME/javadoc
+cp -Rf $HOME/target/apidocs projects/$PROJECT_NAME/javadoc
 git add -f .
 git commit -m "Latest JavaDoc for $PROJECT_NAME #$TRAVIS_BUILD_NUMBER
 Latest JavaDoc on a successful Travis CI build, pushed automatically"
