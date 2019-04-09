85c9eaf38c5116a5210439d284545b596e357cc1
MrExplode <pjanos@outlook.hu>
2019-04-09 20:56:23 +0200

    Fix #10
    Let's see

--- scripts/javadoc.sh
+++ scripts/javadoc.sh
@@ -12,7 +12,7 @@ PROJECT_NAME="DeployTest"
 ###################################
 # Beginning of the script
 ###################################
-echo -e "\nBuilding JavaDoc...\n"
+echo -e "\n\e[93mBuilding JavaDoc...\n"
 
 # Building the javadoc
 mvn javadoc:javadoc
@@ -23,18 +23,15 @@ git config --global user.name "ExplodeBot"
 git config --global user.email "sunstorm@outlook.hu"
 
 # Cloning webpage
-fold_start clone "cloning webpage"
 git clone --branch=master https://${GITHUB_TOKEN}@github.com/MrExplode/MrExplode.github.io website
-fold_end clone
 
 cd website
 git rm -rf ./projects/$PROJECT_NAME/javadoc
-cp -Rf $HOME/target/apidocs ./projects/$PROJECT_NAME/javadoc
+cp -Rf $HOME/target/apidocs /projects/$PROJECT_NAME/javadoc
 git add -f .
 git commit -m "Latest JavaDoc for $PROJECT_NAME #$TRAVIS_BUILD_NUMBER
 Latest JavaDoc on a successful Travis CI build, pushed automatically"
-fold_start push "pushing changes"
+echo -e "\e[93mPushing JavaDoc to webpage"
 git push -fq origin master
-fold_end push
 
 exit 0
